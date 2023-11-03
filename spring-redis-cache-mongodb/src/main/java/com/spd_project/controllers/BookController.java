92610f95f2541924ac7f6c32aeca6f3a5e5e335e
Pavel Bogdan <78963505+pavelbogdancostin@users.noreply.github.com>
2023-11-03 19:03:48 +0200

    Add files via upload

---
+++ spring-redis-cache-mongodb/src/main/java/com/spd_project/controllers/BookController.java
@@ -0,0 +1,43 @@
+package com.spd_project.controllers;
+
+import com.spd_project.model.Book;
+import com.spd_project.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+//@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class BookController {
+
+    private final BookService bookService;
+
+    @PostMapping("/books")
+    public void save(@RequestBody Book book){
+        bookService.save(book);
+    }
+
+    @GetMapping("/books")
+    public List<Book> findAll(){
+        return bookService.findAll();
+    }
+
+    @GetMapping("/books/{isbn}")
+    public Book findByIsbn(@PathVariable long isbn){
+        return bookService.findByIsbn(isbn).get();
+    }
+
+    @DeleteMapping("/books/{isbn}")
+    public void deleteByIsbn(@PathVariable long isbn){
+        bookService.deleteByIsbn(isbn);
+    }
+
+    @PutMapping("books")
+    public void update(@RequestBody Book book){
+        bookService.save(book);
+    }
+
+
+}
