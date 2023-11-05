044f3909ac1a5d21f8bc45bfbd0f88ef519d6041
Pavel Bogdan <78963505+pavelbogdancostin@users.noreply.github.com>
2023-11-05 21:09:40 +0200

    Delete spring-redis-cache-mongodb directory

--- spring-redis-cache-mongodb/src/main/java/com/spd_project/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spd_project.repository;
-
-import com.spd_project.model.Book;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends MongoRepository <Book, Long>{
-}
