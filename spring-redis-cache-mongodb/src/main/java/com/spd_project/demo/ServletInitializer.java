34f99120b6204150b802faf084b0781795a40dd8
Pavel Bogdan <78963505+pavelbogdancostin@users.noreply.github.com>
2023-11-05 20:30:36 +0200

    Add files via upload

---
+++ spring-redis-cache-mongodb/src/main/java/com/spd_project/demo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.spd_project.demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringRedisCacheMongodbApplication.class);
+	}
+
+}
