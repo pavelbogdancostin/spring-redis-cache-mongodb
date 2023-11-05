3a4b9b2513e61c9be000c4b3fdb64d0fc3fe6005
Pavel Bogdan <78963505+pavelbogdancostin@users.noreply.github.com>
2023-11-05 21:11:02 +0200

    Add files via upload

---
+++ spring-redis-cache-mongodb/src/main/java/com/spd_project/SpringRedisCacheMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.spd_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisCacheMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRedisCacheMongodbApplication.class, args);
+	}
+
+}
---
+++ spring-redis-cache-mongodb/src/test/java/com/spd_project/demo/SpringRedisCacheMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spd_project.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisCacheMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
